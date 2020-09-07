eab8d7079bc410bda6dc31daa02f4f647930a63a
mauraqp <mauraqoonitah@gmail.com>
2020-09-07 22:23:41 +0700

    tambah template page berita

---
+++ application/views/berita/index.php
@@ -0,0 +1,127 @@
+<div class="container">
+    <h3 class="mt-3 text-center p-5">Berita Internal di Lingkungan PAM Jaya</h3>
+    <div class="page">
+        <div class="page-berita">
+            <article class="berita">1
+                <hr class="image">
+                <hr>
+            </article>
+            <article class="berita">2
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">3
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">4
+                <hr class="image">
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">5
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">6
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">7
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">8
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">9
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">10
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">11
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">12
+                <hr class="image">
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">13
+                <hr class="image">
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">14
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">15
+                <hr class="image">
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">16
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">17
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">18
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">19
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">20
+                <hr>
+                <hr>
+                <hr>
+            </article>
+            <article class="berita">21
+                <hr>
+                <hr>
+                <hr>
+            </article>
+        </div>
+    </div>
+</div>
