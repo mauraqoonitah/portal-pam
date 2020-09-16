a14c862d1ce0e80b318315412945cc394e63231d
mauraqp <mauraqoonitah@gmail.com>
2020-09-16 22:48:07 +0700

    update modals

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -10,33 +10,41 @@
                     </a>
                     <h3 class="item-title"><?= $item['nama']; ?></h3>
                     <div class="overlay">
-                        <button type="button" class="btn" data-toggle="modal" data-target="#item-modal" ">
+                        <button type="button" class="btn" data-toggle="modal" data-target="#item-modal" data-whatever="<?= $item['nama']; ?>" data-whatever2="<?= $item['deskripsi']; ?>">
                             <i class=" fas fa-info-circle"></i>
                         </button>
                     </div>
                 </div>
 
             </div>
-        <?php endforeach; ?>
 
+
+
+            <!-- Modal -->
+
+            <div class="modal fade" id="item-modal" tabindex="-1" role="dialog" aria-labelledby="item-modalTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle"><?= $item['nama']; ?></h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <?= $item['deskripsi']; ?>
+                        </div>
+                        <div class="modal-footer">
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div> <?php endforeach; ?>
 
     </section>
-    <!-- <section class="settings">
-        <h3 class="title mb-5"> Pengaturan</h3>
-
-        <div class="services">
-            <div class="box">
-                <div class="item">
-                    <a href="http://portal.pamjaya.co.id/ganti-pass.php" target="_blank">
-                        <img class="item-logo pw" src="<?= base_url('assets/img/change password.png'); ?>" alt="ubah password">
-                    </a>
-                    <h2 class="item-title">Ubah password</h2>
-                </div>
-            </div>
-
-
-    </section> -->
-
     <div class="row mt-4 ml-4">
         <div class="col text-left">
             <form action="http://portal.pamjaya.co.id/ganti-pass.php" method="get" target="_blank">
@@ -45,26 +53,3 @@
         </div>
 
     </div>
-    <!-- Modal -->
-    <div class="modal fade" id="item-modal" tabindex="-1" role="dialog" aria-labelledby="item-modalTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Deskripsi</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam repellendus asperiores consequatur blanditiis ex, incidunt natus necessitatibus eum pariatur accusamus. Soluta voluptate quos incidunt atque accusamus error commodi excepturi qui illum non ducimus, culpa odio, facere perferendis quibusdam quod corporis fugit veritatis iusto deserunt. Veritatis, rerum! Modi pariatur at ea!
-                </div>
-                <div class="modal-footer">
-
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-</div>
