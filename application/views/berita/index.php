8c10759cb8d4658e1af7ba4f45c912a6c0b19e41
mauraqp <mauraqoonitah@gmail.com>
2020-09-18 18:12:32 +0700

    bikin database berita

--- application/views/berita/index.php
+++ application/views/berita/index.php
@@ -1,128 +1,21 @@
 <div class="container">
-    <h3 class="mt-3 text-center p-5">Berita Internal di Lingkungan PAM Jaya</h3>
+    <h3 class="mt-3 text-center p-5">Berita Internal di Lingkungan PAM JAYA</h3>
     <div class="page">
         <div class="page-berita">
-            <article class="berita">1
-                <h6 class="card-subtitle mb-2 text-muted">12/06/2020</h6>
-                <h4> Penggunaan Alfresco untuk seluruh karyawan PAM JAYA</h4>
-            </article>
+
             <article class="berita">2
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis eveniet quos doloremque corporis! Dicta vitae minima recusandae distinctio ipsam non, expedita nobis commodi suscipit accusamus voluptate, illo est voluptatem. Libero rem, consequuntur eius vero unde facilis nostrum corrupti natus dignissimos atque, assumenda recusandae ad commodi minus enim pariatur dolorem dicta. </p>
+                <div class="ilang">
+                    <h6 class="card-subtitle mb-2 text-muted ilang">12/06/2020 | Admin</h6>
+
+                    <h4> Penggunaan Alfresco untuk seluruh karyawan PAM JAYA</h4>
+                </div>
+                <div class="hide">
+                    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis eveniet quos doloremque corporis! Dicta vitae minima recusandae distinctio ipsam non, expedita nobis commodi suscipit accusamus voluptate, illo est voluptatem. Libero rem, consequuntur eius vero unde facilis nostrum corrupti natus dignissimos atque, assumenda recusandae ad commodi minus enim pariatur dolorem dicta. </p>
+                    <button type="button" class="btn btn-dark">read more</button>
+                </div>
             </article>
-            <article class="berita">3
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">4
-                <hr class="image">
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">5
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">6
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">7
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">8
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">9
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">10
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">11
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">12
-                <hr class="image">
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">13
-                <hr class="image">
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">14
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">15
-                <hr class="image">
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">16
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">17
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">18
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">19
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">20
-                <hr>
-                <hr>
-                <hr>
-            </article>
-            <article class="berita">21
-                <hr>
-                <hr>
-                <hr>
-            </article>
+
+
         </div>
     </div>
 </div>
