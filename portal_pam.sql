e9d6ff6f5834b3b38a7022dd80fa019ddead5e32
mauraqp <mauraqoonitah@gmail.com>
2020-09-18 18:20:00 +0700

    database update

--- portal_pam.sql
+++ portal_pam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2020 at 02:31 PM
+-- Generation Time: Sep 18, 2020 at 01:13 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,6 +25,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `berita`
+--
+
+CREATE TABLE `berita` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `deskripsi` varchar(100) NOT NULL,
+  `publishedAt` varchar(100) NOT NULL,
+  `konten` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `berita`
+--
+
+INSERT INTO `berita` (`id`, `judul`, `gambar`, `deskripsi`, `publishedAt`, `konten`) VALUES
+(1, 'Bitcoin speculators hit all-time lows as Grayscale says BTC like 2016', 'test.jpg', 'Data says hodlers are gaining and speculators are vanishing from Bitcoin, with one commentator argui', '01/09/2020', 'konten1'),
+(2, 'Blockchain for Decision Makers: A systematic guide to using blockchain for improving your business-P', 'test.jpg', 'In addition to cryptocurrencies, blockchain-based apps are being developed in different industries s', '02/09/2020', 'konten2'),
+(3, 'Crypto Stimulus: How Ethereum Based Uniswap Gave All Its Users Nearly $1,200', 'test.jpg', 'It has been a challenging two weeks in the cryptocurrency market for both major and minor coins. Bit', '03/09/2020', 'konten3'),
+(4, 'Crypto Stimulus: How Ethereum Based Uniswap Gave All Its Users Nearly $1,200', 'test.jpg', 'From creative briefs and awards entries to press releases and thinkpieces, the updated Bland Book is', '04/09/2020', 'konten4'),
+(5, 'Free 400 UNI tokens for Crypto Traders $1000 value', 'test.jpg', 'THis is for people who trade crypto or have in the past.Uniswap is giving 400 UNI tokens away for th', '05/09/2020', 'konten5'),
+(6, 'BiggerPockets Podcast 400: “This Advice Changed My Life” with Brandon and David', 'test.jpg', 'THis is for people who trade crypto or have in the past. \\n \\nUniswap is giving 400 UNI tokens away ', '06/09/2020', 'konten6'),
+(7, '3 Biggest Takeaways for Bitcoin From Powell’s Press Conference', 'test.jpg', 'The Bitcoin price popped above $11,000 just as the Federal Reserve Chairman Jerome Powell started de', '07/09/2020', 'konten7'),
+(8, 'Crypto Stimulus: How Ethereum Based Uniswap Gave All Its Users Nearly $1,200', 'test.jpg', 'Ethereum based Uniswap gave 400 UNI to all users that used the decentralized exchange prior to ... [', '08/09/2020', 'konten8');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `item`
 --
 
@@ -41,7 +70,7 @@ CREATE TABLE `item` (
 --
 
 INSERT INTO `item` (`id`, `nama`, `icon`, `link`, `deskripsi`) VALUES
-(5, 'Alfresco', 'alfresco-icon.png', 'https://docs.pamjaya.co.id/', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto ducimus quaerat nostrum dignissimos, natus, nihil sit esse, ut atque laudantium dolores modi non soluta aut amet facilis quis doloremque perferendis placeat culpa iste at! Doloremque consectetur placeat adipisci, error sapiente atque. Voluptates incidunt ullam minus, consequuntur tempore accusantium saepe quaerat.'),
+(5, 'Alfresco', 'alfresco-icon.png', 'https://docs.pamjaya.co.id/', 'alfresco Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto ducimus quaerat nostrum dignissimos, natus, nihil sit esse, ut atque laudantium dolores modi non soluta aut amet facilis quis doloremque perferendis placeat culpa iste at! Doloremque consectetur placeat adipisci, error sapiente atque. Voluptates incidunt ullam minus, consequuntur tempore accusantium saepe quaerat.'),
 (7, 'TNDE', 'tnde-icon.png', 'https://mail.pamjaya.co.id/', 'TNDE Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto ducimus quaerat nostrum dignissimos, natus, nihil sit esse, ut atque laudantium dolores modi non soluta aut amet facilis quis doloremque perferendis placeat culpa iste at! Doloremque consectetur placeat adipisci, error sapiente atque. Voluptates incidunt ullam minus, consequuntur tempore accusantium saepe quaerat.'),
 (9, 'e-mail', 'zimbra-icon.png', 'https://mail.pamjaya.co.id/', 'zimbra Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, minima adipisci odio, est optio ipsum iure officia amet hic eaque laborum unde, nulla magni explicabo. Nam obcaecati deleniti sit molestiae voluptate vero aliquid optio, eos quia possimus, impedit, voluptatem dicta totam itaque earum facere dolorum id eveniet perferendis voluptatibus veritatis!'),
 (11, 'HRIS', 'hris-icon.png', 'https://sf.dataon.com/sf6/index.cfm', 'HRIS Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, minima adipisci odio, est optio ipsum iure officia amet hic eaque laborum unde, nulla magni explicabo. Nam obcaecati deleniti sit molestiae voluptate vero aliquid optio, eos quia possimus, impedit, voluptatem dicta totam itaque earum facere dolorum id eveniet perferendis voluptatibus veritatis!'),
@@ -50,11 +79,25 @@ INSERT INTO `item` (`id`, `nama`, `icon`, `link`, `deskripsi`) VALUES
 (14, 'SIL', 'sil-icon.png', 'http://portal.pamjaya.co.id/akses-sil.php', 'SIL Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, minima adipisci odio, est optio ipsum iure officia amet hic eaque laborum unde, nulla magni explicabo. Nam obcaecati deleniti sit molestiae voluptate vero aliquid optio, eos quia possimus, impedit, voluptatem dicta totam itaque earum facere dolorum id eveniet perferendis voluptatibus veritatis!'),
 (15, 'Kimonev', 'kimonev-icon.png', 'https://kimonev.pamjaya.co.id/', 'kimonev Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, minima adipisci odio, est optio ipsum iure officia amet hic eaque laborum unde, nulla magni explicabo. Nam obcaecati deleniti sit molestiae voluptate vero aliquid optio, eos quia possimus, impedit, voluptatem dicta totam itaque earum facere dolorum id eveniet perferendis voluptatibus veritatis!'),
 (16, 'MAYA', 'maya-icon.png', 'http://203.161.27.194:3821/sim_aset/', 'maya Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, minima adipisci odio, est optio ipsum iure officia amet hic eaque laborum unde, nulla magni explicabo. Nam obcaecati deleniti sit molestiae voluptate vero aliquid optio, eos quia possimus, impedit, voluptatem dicta totam itaque earum facere dolorum id eveniet perferendis voluptatibus veritatis!'),
-(17, 'MONICA', 'monica-icon.png', 'https://monica.pamjaya.co.id/', 'monica Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, minima adipisci odio, est optio ipsum iure officia amet hic eaque laborum unde, nulla magni explicabo. Nam obcaecati deleniti sit molestiae voluptate vero aliquid optio, eos quia possimus, impedit, voluptatem dicta totam itaque earum facere dolorum id eveniet perferendis voluptatibus veritatis!');
+(17, 'MONICA', 'monica-icon.png', 'https://monica.pamjaya.co.id/', 'monica Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, minima adipisci odio, est optio ipsum iure officia amet hic eaque laborum unde, nulla magni explicabo. Nam obcaecati deleniti sit molestiae voluptate vero aliquid optio, eos quia possimus, impedit, voluptatem dicta totam itaque earum facere dolorum id eveniet perferendis voluptatibus veritatis!'),
+(20, 'test', 'footer.gif', 'https://www.instagram.com/mauraputri/', 'ini lagi test ya allah semoga bisaaa'),
+(21, 'test2', 'test2.png', 'materializecss.com', 'ini lagi test kedua ya allah semoga bisaaa'),
+(22, 'test3 iniii ', 'test3.png', '', 'ini test ketiga ya allah semoga bisaaaa'),
+(23, 'test4 nih huhaah', 'test4.jpg', '', 'ini test 4 alhamdulillah'),
+(24, 'test5 neeh', 'test5.png', '', 'test5 niii'),
+(25, 'test6', 'test6.jpg', '', 'test6'),
+(26, 'test7', 'test7.jpg', '', 'test7'),
+(27, 'test8', 'test8.jpg', '', 'test8');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `berita`
+--
+ALTER TABLE `berita`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `item`
@@ -67,10 +110,16 @@ ALTER TABLE `item`
 --
 
 --
+-- AUTO_INCREMENT for table `berita`
+--
+ALTER TABLE `berita`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
