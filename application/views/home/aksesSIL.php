4d76ad1d9f0180405d5ece4de42b3ff9aa789c98
mauraqp <mauraqoonitah@gmail.com>
2020-10-05 00:41:50 +0700

    fix responsive layout

--- application/views/home/aksesSIL.php
+++ application/views/home/aksesSIL.php
@@ -1,7 +1,11 @@
 <div class="container">
-    <div class="mx-auto mt-5 text-center" style="width: 500px;">
-        <h4> Maaf, Aplikasi Sistem Informasi Laboratorium hanya dapat diakses dalam lingkungan kantor PAMJAYA</h4>
-        <h5 class="mt-3">IP: 180.243.191.215</h5>
 
+    <div class="row">
+        <div class="col-md-6 offset-md-3 text-center mt-5">
+            <h4> Maaf, Aplikasi Sistem Informasi Laboratorium hanya dapat diakses dalam lingkungan kantor PAMJAYA</h4>
+            <h5 class="mt-3">IP: 180.243.191.215</h5>
+        </div>
     </div>
+
+
 </div>
--- application/views/home/ubahpassword.php
+++ application/views/home/ubahpassword.php
@@ -1,7 +1,10 @@
 <div class="container">
-    <div class="mx-auto mt-5 text-center" style="width: 500px;">
-        <h4> Maaf, Pergantian Password hanya dapat diakses dalam lingkungan kantor PAMJAYA</h4>
-        <p>Bila mendesak, anda dapat menghubungi SIM di pesawat +62215704250</p>
 
+    <div class="row">
+        <div class="col-md-6 offset-md-3 text-center mt-5">
+            <h4> Maaf, Pergantian Password hanya dapat diakses dalam lingkungan kantor PAMJAYA</h4>
+            <h6 class="font-italic">Bila mendesak, anda dapat menghubungi SIM di pesawat +62215704250</h6>
+        </div>
     </div>
+
 </div>
